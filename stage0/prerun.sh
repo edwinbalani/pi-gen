df0ddb0c123d2dddb9cadef0004edd4f80065d87
Edwin Balani <git@balani.xyz>
2020-06-13 11:51:45 +0100

    Use the right repositories for arm64 (oops)
    Turns out raspbian multiarch repos are definitely not the right place.

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -6,7 +6,7 @@ if [ ! -d "${ROOTFS_DIR}" ]; then
 			bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
 			;;
 		arm64)
-			bootstrap --verbose ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/multiarch/
+			bootstrap --include=gnupg ${RELEASE} "${ROOTFS_DIR}" http://deb.debian.org/debian/
 			;;
 	esac
 fi
