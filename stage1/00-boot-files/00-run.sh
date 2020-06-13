25aa53f0ea6492c774edf38dae77810e87cad45d
Edwin Balani <git@balani.xyz>
2020-06-14 00:39:06 +0100

    Remember to enable 64-bit boot for arm64 builds!
    This commit also incorporates changes to the stock /boot/config.txt that
    seem to have been made upstream -- namely, the DRM VC4 V3D driver is now
    enabled on all boards, not just the Pi 4.

--- stage1/00-boot-files/00-run.sh
+++ stage1/00-boot-files/00-run.sh
@@ -2,3 +2,7 @@
 
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
+
+if [ "${TARGET_ARCH}" = "arm64" ]; then
+	echo 'arm_64bit=1' >> "${ROOTFS_DIR}/boot/config.txt"
+fi
