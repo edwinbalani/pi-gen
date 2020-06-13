d947c64e4769538e7273a6d505bda74f2acf60d1
Edwin Balani <git@balani.xyz>
2020-06-13 12:47:16 +0100

    Only install raspi-copies-and-fills on armhf

--- stage2/00-copies-and-fills/00-run.sh
+++ stage2/00-copies-and-fills/00-run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
+if [ "${TARGET_ARCH}" = "armhf" ]; then
+	on_chroot << EOF
+apt-get -o APT::Acquire::Retries=3 install -y raspi-copies-and-fills
+EOF
+fi
 
 if [ -f "${ROOTFS_DIR}/etc/ld.so.preload" ]; then
    mv "${ROOTFS_DIR}/etc/ld.so.preload" "${ROOTFS_DIR}/etc/ld.so.preload.disabled"
 fi
-
